0dd380bf8a67a95800ef8d5c6650545d7e28a13e
Ankit Kisi <ankitkisi@Ankits-MacBook-Air.local>
2023-08-17 13:33:24 +0545

    sdl graph

--- Makefile
+++ Makefile
@@ -1,20 +1,44 @@
-# Compiler settings
-CC := gcc
-CFLAGS := -Wall -Wextra -g
+# # Compiler settings
+# CC := gcc
+# CFLAGS := -Wall -Wextra -g
 
-# Source files and object files
-SRCS := photo.c ml.c opener.c
-OBJS := $(SRCS:.c=.o)
-EXECUTABLE := run
+# # Source files and object files
+# SRCS := photo.c ml.c opener.c
+# OBJS := $(SRCS:.c=.o)
+# EXECUTABLE := run
 
-# Main target (executable)
-$(EXECUTABLE): $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) -o $(EXECUTABLE) -lm
+# # Main target (executable)
+# $(EXECUTABLE): $(OBJS)
+# 	$(CC) $(CFLAGS) $(OBJS) -o $(EXECUTABLE) -lm
 
-# Rule to compile C files into object files
+# # Rule to compile C files into object files
+# %.o: %.c
+# 	$(CC) $(CFLAGS) -c $< -o $@
+
+# # Clean rule to remove object files and the executable
+# clean:
+# 	rm -f $(OBJS) $(EXECUTABLE)
+
+
+##make file for kisi
+
+CC = gcc
+CFLAGS = -Wall -Wextra -g
+FRAMEWORKS = -framework SDL2 -framework SDL2_ttf
+INCLUDE_DIRS = -I/Users/ankitkisi/Desktop/supervised-ML-with-c/SDL2.framework/Headers -I/Users/ankitkisi/Desktop/supervised-ML-with-c/SDL2_ttf.framework/Headers
+FRAMEWORK_DIRS = -F/Library/Frameworks
+
+SRC_FILES = opener.c photo.c ml.c
+OBJ_FILES = $(SRC_FILES:.c=.o)
+EXECUTABLE = run
+
+all: $(EXECUTABLE)
+
+$(EXECUTABLE): $(OBJ_FILES)
+	$(CC) $(FRAMEWORKS) $(FRAMEWORK_DIRS) $(OBJ_FILES) -o $(EXECUTABLE)
+
 %.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) $(INCLUDE_DIRS) -c $< -o $@
 
-# Clean rule to remove object files and the executable
 clean:
-	rm -f $(OBJS) $(EXECUTABLE)
+	rm -f $(OBJ_FILES) $(EXECUTABLE)
--- ml.c
+++ ml.c
@@ -2,12 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
+//graph header files
+#include <SDL2/SDL.h>
+#include <SDL_ttf.h>
+
+//graph
+// Screen dimensions
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 800;
+
+// Graph dimensions and parameters
+const int GRAPH_WIDTH = 1000;
+const int GRAPH_HEIGHT = 500;
+const int GRAPH_X = 60;//(SCREEN_WIDTH - GRAPH_WIDTH) / 2;
+const int GRAPH_Y = 700; //(SCREEN_HEIGHT - GRAPH_HEIGHT) / 0.2;
+const int NUM_VALUES = GRAPH_WIDTH; // Number of values to display
+const double SCALE_X = 0.9; // X-axis scaling factor
+const double SCALE_Y = 20; // Y-axis scaling factor
+
+//current position of x coordinate
+int currentX = 0;
 
 
+//ML code
 int p = 0;
-
-
-
 
 int count;
 
@@ -123,6 +141,83 @@ void gradiant(int max_cols,double w[max_cols-1], double b,double rate,double ran
 
 }
 
+void drawgraph(double values[NUM_VALUES], int p, SDL_Window* window, SDL_Renderer* renderer ){
+    // Clear the screen
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw axes
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawLine(renderer, GRAPH_X - 10, GRAPH_Y, GRAPH_X - 10, GRAPH_Y - GRAPH_HEIGHT);
+        SDL_RenderDrawLine(renderer, GRAPH_X - 10, GRAPH_Y, GRAPH_X + GRAPH_WIDTH, GRAPH_Y);
+
+        // Draw graph
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        for (int i = 1; i < currentX; i++) {
+            int x1 = GRAPH_X + (i - 1)* SCALE_X;
+            int y1 = GRAPH_Y - (int)(values[i - 1] * SCALE_Y);
+            int x2 = GRAPH_X + i * SCALE_X;
+            int y2 = GRAPH_Y - (int)(values[i] * SCALE_Y);
+            SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+        }
+
+        // Draw epoch and cost values on top
+        
+
+        char epochText[20];
+        char costText[20];
+        snprintf(epochText, sizeof(epochText), "Epoch: %d", p);
+        snprintf(costText, sizeof(costText), "Cost: %lf", values[currentX - 1]);
+
+        SDL_Color textColor = {0, 0, 0, 255};
+
+         //open font
+        TTF_Font* Sans = TTF_OpenFont("OpenSans-Regular.ttf", 24);
+        if (!Sans) {
+            SDL_Log("Failed to open font: %s", TTF_GetError());
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+           
+        }
+        
+        SDL_Surface* epochSurface = TTF_RenderText_Solid(Sans, epochText, textColor);
+        SDL_Surface* costSurface = TTF_RenderText_Solid(Sans, costText, textColor);
+
+        SDL_Texture* epochTexture = SDL_CreateTextureFromSurface(renderer, epochSurface);
+        SDL_Texture* costTexture = SDL_CreateTextureFromSurface(renderer, costSurface);
+
+        int epochWidth, epochHeight;
+        SDL_QueryTexture(epochTexture, NULL, NULL, &epochWidth, &epochHeight);
+        int costWidth, costHeight;
+        SDL_QueryTexture(costTexture, NULL, NULL, &costWidth, &costHeight);
+
+        SDL_Rect epochRect = {SCREEN_WIDTH / 2 - epochWidth / 2, 700, epochWidth, epochHeight};
+        SDL_Rect costRect = {140 / 2, 30 + epochHeight, costWidth, costHeight};
+
+        SDL_RenderCopy(renderer, epochTexture, NULL, &epochRect);
+        SDL_RenderCopy(renderer, costTexture, NULL, &costRect);
+
+        SDL_DestroyTexture(epochTexture);
+        SDL_DestroyTexture(costTexture);
+        SDL_FreeSurface(epochSurface);
+        SDL_FreeSurface(costSurface);
+
+        // Close the font at the end of each loop iteration
+        TTF_CloseFont(Sans);
+        
+        // Introduce a delay of 100 milliseconds (adjust as needed)
+        //SDL_Delay(10); // Delay for 10 milliseconds
+
+       
+
+        // Render to screen
+        SDL_RenderPresent(renderer);
+
+}
+
+
+
 
   
 void max_find(int TRAIN_ROWS ,int n,float train[TRAIN_ROWS][n],double max[n])
@@ -179,6 +274,38 @@ void min_find(int TRAIN_ROWS ,int n,float train[TRAIN_ROWS][n],double min[n])
 float* linear_regression_model(int c,int max_cols, float train[c][max_cols],int epoch,double rate ,double range_max,
     double range_min )
 {
+    
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SUPERVISED ML", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if (!window) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        SDL_Quit();
+        
+    }
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        
+    }
+    // Initialize SDL_TTF
+    if (TTF_Init() != 0) {
+        SDL_Log("Unable to initialize SDL_TTF: %s", TTF_GetError());
+        SDL_Quit();
+        
+    }
+    double values[NUM_VALUES] = {0.0};
+
+    //ML code
     count = c;
     srand(10);    
     double w[max_cols];
@@ -187,6 +314,7 @@ float* linear_regression_model(int c,int max_cols, float train[c][max_cols],int 
         w[i] = 0*(rand()/RAND_MAX)/2;
     }
     double b = 0;
+    
     
     double max[max_cols];
     
@@ -206,11 +334,63 @@ float* linear_regression_model(int c,int max_cols, float train[c][max_cols],int 
 min_find( c, max_cols,train,max);
 max_find( c, max_cols,train,max);
 
-p=0;
-    for (int i = 0; i < epoch; i++)
-    {
+while (p<epoch) {
+        
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                p = epoch;
+            }
+        }
+
+        
+       
+        
+
+
+//p=0;
+    
         gradiant( max_cols,w,b,rate,range_max,range_min, train,max);
-    }
+    
+
+
+    if (p%(epoch / 100000) == 0 || p == 1 )
+        {
+            // Get live cost values
+            if (currentX < NUM_VALUES) {
+            
+            values[currentX] = rmse(count,max_cols,w,b,train);
+            currentX++;
+            }
+            else {
+            // Shift data to accommodate new value
+            for (int i = 0; i < NUM_VALUES - 1; i++) {
+                values[i] = values[i + 1];
+            }
+            values[NUM_VALUES - 1] = rmse(count,max_cols,w,b,train);
+        }
+
+            //Draw graph function call
+            drawgraph(values, p ,window, renderer);
+
+        }
+        
+
+
+
+}
+
+
+
+
+
+    // Cleanup and exit
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    SDL_Quit();
+
+
 
     float *wb = (float*)malloc(max_cols*sizeof(int));
     for (int i =0; i<max_cols-1;i++)
@@ -220,5 +400,6 @@ p=0;
     wb[max_cols-1]= b;
     printf("--\n");
     return wb;
+
 
 }
